c0e8a842852e671b5619869f70a3a52247f3c60a
shashank boudh <69084974+LazzyAF@users.noreply.github.com>
2020-09-11 16:07:13 +0530

    Update app.js

--- static/app.js
+++ static/app.js
@@ -27,7 +27,7 @@ function onClickedEstimatePrice() {
   var estPrice = document.getElementById("uiEstimatedPrice");
 
  // var url = "http://127.0.0.1:5000/predict_home_price"; 
-    var url = "/predict_home_price"; 
+    var url = "https://github.com/LazzyAF/House_price_pred/blob/master/app.py/predict_home_price"; 
 
   $.post(url, {
       total_sqft: parseFloat(sqft.value),
@@ -44,7 +44,7 @@ function onClickedEstimatePrice() {
 function onPageLoad() {
   console.log( "document loaded" );
   //var url = "http://127.0.0.1:5000/get_location_names"; // 
-    var url = "/get_location_names"; 
+    var url = "https://github.com/LazzyAF/House_price_pred/blob/master/app.py/get_location_names"; 
   $.get(url,function(data, status) {
       console.log("got response for get_location_names request");
       if(data) {
