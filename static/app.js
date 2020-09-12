1bde6c7f0fa1f552a11ce6d0ffbf9b276761ea94
shashank boudh <69084974+LazzyAF@users.noreply.github.com>
2020-09-12 18:15:24 +0530

    Update app.js

--- static/app.js
+++ static/app.js
@@ -26,7 +26,7 @@ function onClickedEstimatePrice() {
   var location = document.getElementById("uiLocations");
   var estPrice = document.getElementById("uiEstimatedPrice");
 
-  var url = "https://api/predict_home_price"; 
+  var url = "https://git.heroku.com/bhspp.git/predict_home_price"; 
 
   $.post(url, {
       total_sqft: parseFloat(sqft.value),
@@ -42,7 +42,7 @@ function onClickedEstimatePrice() {
 
 function onPageLoad() {
   console.log( "document loaded" );
-    var url = "https://api/get_location_names"; 
+    var url = "https://git.heroku.com/bhspp.git/get_location_names"; 
    
   $.get(url,function(data, status) {
       console.log("got response for get_location_names request");
